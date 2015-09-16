44c2c032560028d5b4f9d92c7d64afcc553bdacd
Travis H <t3patterson@gmail.com>
2015-09-16 08:54:34 -0500

    from codepen to my dev-environment

---
+++ dist/app-browserify.js
@@ -0,0 +1,1 @@
+console.error("SyntaxError: /Users/travish/GithubProjects/Tuts-HotOrCold/js/app-browserify.js: Unexpected token (4:4) while parsing file: /Users/travish/GithubProjects/Tuts-HotOrCold/js/app-browserify.js");
--- js/app-browserify.js
+++ js/app-browserify.js
@@ -1,32 +1,12 @@
-// es5 polyfills, powered by es5-shim
-require("es5-shim")
 
-// es6 polyfills, powered by babel
-require("babel/polyfill")
+var btn = document.querySelector('.guess-button')
 
-var Promise = require('es6-promise').Promise
-var React = require('react');
+val Math.random()*100+1
 
-
-
-// just Node?
-// var fetch = require('node-fetch')
-// Browserify?
-// require('whatwg-fetch') //--> not a typo, don't store as a var
-
-// other stuff that we don't really use in our own code
-// var Pace = require("../bower_components/pace/pace.js")
-
-// require your own libraries, too!
-// var Router = require('./app.js')
-
-window.addEventListener('load', app)
-
-function app() {
-}
-
-
-
+btn.addEventListener("click", function(evt){
+  var userInput = document.querySelector('input').value
+  console.log(parse_int(userInput))
+})
 
 
 
